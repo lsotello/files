c3e0dc04bdb85500731057b5f8461717ad85a117
Luciano Sotello <lsotello@gmail.com>
2025-03-31 20:29:30 -0300

    Bloco Try-with-resources

--- src/bfFile/Program.java
+++ src/bfFile/Program.java
@@ -10,38 +10,17 @@ public class Program {
 
 		String path = "C:\\Users\\lsote\\eclipse-workspace\\bfFile\\in.txt";
 		
-		BufferedReader bf = null;
-		FileReader fr = null;
-		
-		try {
-			fr = new FileReader(path);
-			bf = new BufferedReader(fr);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			
-			String line = bf.readLine();
+			String line = br.readLine();
 			
 			while (line != null) {
 				System.out.println(line);
-				line = bf.readLine();
+				line = br.readLine();
 			}
 		}
 		catch(IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {
-			try {
-				if(bf != null) {
-					bf.close();
-				}
-				
-				if(fr != null) {
-					fr.close();
-				}	
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
-
 	}
-
 }
